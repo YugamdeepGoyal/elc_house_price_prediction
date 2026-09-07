1f161e71c44684931ba3b89f77a2a864ce631036
YugamdeepGoyal <yugamdeep@gmail.com>
2026-09-07 09:00:24 +0530

    Adding preprocessor code

--- src/linear_pipeline.py
+++ src/linear_pipeline.py
@@ -7,17 +7,71 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, StandardScaler
 from sklearn.base import BaseEstimator, TransformerMixin
 
+
 class GeoTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, n_clusters=2, random_state=42):
         self.n_clusters = n_clusters
         self.random_state = random_state
 
     def fit(self, X, y=None):
-        self.kmeans_ = KMeans(n_clusters=self.n_clusters, random_state=self.random_state, n_init=10)
+        self.kmeans_ = KMeans(
+            n_clusters=self.n_clusters, random_state=self.random_state, n_init=10
+        )
         self.kmeans_.fit(X[["longitude", "latitude"]])
         return self
 
     def transform(self, X):
         X = X.copy()
-        X['geo_cluster'] = self.kmeans_.predict(X[["longitude", "latitude"]])
+        X["geo_cluster"] = self.kmeans_.predict(X[["longitude", "latitude"]])
         return X
+
+
+def skew_fix(X):
+    X = X.copy()
+    numeric_cols = [
+        "total_rooms",
+        "total_bedrooms",
+        "population",
+        "households",
+        "median_income",
+    ]
+    for col in numeric_cols:
+        X[col] = np.log1p(X[col])
+    return X
+
+
+def binning(X):
+    X = X.copy()
+    X["age_bin"] = pd.cut(
+        X["housing_median_age"],
+        bins=[0, 15, 30, 45, 52],
+        labels=["new", "mid", "old", "very_old"],
+        include_lowest=True,
+    )
+    return X
+
+
+def get_preprocessor():
+    numeric_features = [
+        "longitude",
+        "latitude",
+        "total_rooms",
+        "total_bedrooms",
+        "population",
+        "households",
+        "median_income",
+    ]
+
+    categorical_features = ["ocean_proximity", "geo_cluster", "age_bin"]
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", Pipeline([
+                ("imputer", SimpleImputer(strategy="median")),
+                ("scaler", StandardScaler())
+            ]), numeric_features),
+
+            ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_features)
+        ]
+    )
+    return preprocessor
